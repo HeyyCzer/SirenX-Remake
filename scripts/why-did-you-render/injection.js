acf1fb3ad81ea0b4319f140a6d5c28102bcad8ca
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2024-02-20 20:01:28 -0300

    Corrigido erro do WYDR

--- scripts/why-did-you-render/injection.js
+++ scripts/why-did-you-render/injection.js
@@ -4,23 +4,21 @@ import { useAppSelector } from '@/lib/hooks';
 import whyDidYouRender from '@welldone-software/why-did-you-render';
 import { useSelector } from 'react-redux';
 	
-if (process.env.WDYR_ENABLED === 'true') {
-	// eslint-disable-next-line no-console -- Show information that `whyDidYouRender` has been applied to the website.
-	console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
+// eslint-disable-next-line no-console -- Show information that `whyDidYouRender` has been applied to the website.
+console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
 
-	// See https://github.com/welldone-software/why-did-you-render#options
-	whyDidYouRender(React, {
-		trackAllPureComponents: true,
-		trackHooks: true,
-		logOwnerReasons: true,
-		collapseGroups: true,
-		include: [/./],
-		trackExtraHooks: [
-			[useSelector, "useSelector"],
-			[useAppSelector, "useAppSelector"],
-		],
+// See https://github.com/welldone-software/why-did-you-render#options
+whyDidYouRender(React, {
+	trackAllPureComponents: true,
+	trackHooks: true,
+	logOwnerReasons: true,
+	collapseGroups: true,
+	include: [/./],
+	trackExtraHooks: [
+		[useSelector, "useSelector"],
+		[useAppSelector, "useAppSelector"],
+	],
 
-		// This is for testing, remove it, if you don't want to log on different values
-		logOnDifferentValues: true
-	});
-}
+	// This is for testing, remove it, if you don't want to log on different values
+	logOnDifferentValues: false
+});
